861f393ba337eb4c37d6d4aec4faca59c7b88f33
blacksmithalex <blacksmithalex@mail.ru>
2022-06-25 12:01:42 +0300

    new tasks

---
+++ 6 Цикл While /Длина последовательности.py
@@ -0,0 +1,6 @@
+r = int(input())
+count = 0
+while r != 0:
+    count += 1
+    r = int(input())
+print(count)
---
+++ 6 Цикл While /Список квадратов.py
@@ -0,0 +1,7 @@
+N = int(input())
+n = 1
+
+while n**2 <= N:
+    print(n**2)
+    n += 1
+
